45a49b3a8950a3fdb2777c7df459dac7d2dbf16a
clinton-slice <ccnkemdilim@gmail.com>
2020-11-14 00:30:08 -0500

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {selectUser} from "./features/userSlice";
 import "./App.css";
 import Imessage from "./components/imessage/IMessage";
+import Login from "./components/login/login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Imessage/>
+      {user ? <Imessage/> : <Login/>}
     </div>
   );
 }
