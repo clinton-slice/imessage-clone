0ff336b82060e498944790e7d0eee94f01230c89
clinton-slice <ccnkemdilim@gmail.com>
2020-11-16 11:11:51 -0500

    Added timeago to project and finishing off sidebarChat component

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -2,15 +2,18 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./message.css";
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 
 const Message = ({
     id, 
     contents: { timestamp, displayName, email, message, photo, uid},
 }) => {
-    console.log(id, displayName, email, uid);
+    const user = useSelector(selectUser);
+    console.log(id, displayName, email, uid, new Date(timestamp?.toDate().toLocaleDateString()));
     return (
-        <div className="message">
-            <Avatar src={photo}/>
+        <div className={`message ${user.email === email && "message_sender"}`}>
+            <Avatar className='message_photo' src={photo}/>
             <p>{message}</p>
             <small>
                 {new Date(timestamp?.toDate()).toLocaleDateString()}
